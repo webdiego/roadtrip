f639355c52ad82fd463a73f5333f022e5a183ef6
Diego Massarni <webdiegomassarini@gmail.com>
2024-05-16 23:08:44 +1000

    update db

--- drizzle/0000_narrow_puff_adder.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `trip` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`user_id` text NOT NULL,
-	`title` text NOT NULL,
-	`description` text NOT NULL,
-	`timestamp` integer DEFAULT (unixepoch()) NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `trip_user_id_unique` ON `trip` (`user_id`);
--- src/db/schema/trips.ts
+++ src/db/schema/trips.ts
@@ -1,15 +1,43 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const TripTable = sqliteTable("trip", {
   id: integer("id").primaryKey(),
   userId: text("user_id").notNull(),
   name: text("title").notNull(),
-  description: text("description").notNull(),
+  description: text("description"),
+  budget: integer("budget").default(0),
+  amount_used: integer("amount_used").default(0),
+  currency: text("currency").default("USD"),
+  status: text("status").default("active"),
   createdAt: integer("timestamp", { mode: "number" })
     .notNull()
-    .default(sql`(unixepoch())`), // Date
+    .default(sql`(unixepoch())`),
 });
 
+export const usersRelations = relations(TripTable, ({ one }) => ({
+  expenses: one(ExpensesTable),
+}));
+
+export const ExpensesTable = sqliteTable("expenses", {
+  id: integer("id").primaryKey(),
+  tripId: integer("trip_id").references(() => TripTable.id),
+  type: text("type", {
+    enum: [
+      "food",
+      "petrol",
+      "transportation",
+      "lodging",
+      "pleasure",
+      "sport",
+      "other",
+    ],
+  }),
+  description: text("description"),
+  amount: integer("amount").notNull(),
+  createdAt: integer("timestamp", { mode: "number" })
+    .notNull()
+    .default(sql`(unixepoch())`),
+});
 export type InsertHTrip = typeof TripTable.$inferInsert;
 export type SelectHaTrip = typeof TripTable.$inferSelect;
