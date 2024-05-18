9af2b17555fb5cc1ad2c3c8255e51baad34979f2
Diego Massarni <webdiegomassarini@gmail.com>
2024-05-18 11:46:40 +1000

    wip

--- src/pages/trips/create/index.tsx
+++ src/pages/trips/create/index.tsx
@@ -2,9 +2,31 @@ import React from "react";
 import axios from "axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import * as z from "zod";
+
+const schema = z.object({
+  name: z.string().min(1, { message: "Required" }),
+  description: z.string().min(1, { message: "Required" }),
+  budget: z.number().min(1, { message: "Required" }),
+  amount_used: z.number().min(1),
+  currency: z.string().min(1, { message: "Required" }),
+  status: z.string().min(1, { message: "Required" }),
+});
 
 export default function CreateTrip() {
   const queryClient = useQueryClient();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
   // Mutations
   const { isPending, isSuccess, isError, mutate, error } = useMutation({
@@ -20,6 +42,21 @@ export default function CreateTrip() {
   return (
     <div>
       Create trip{" "}
+      <form onSubmit={handleSubmit((d) => console.log(d))}>
+        <Label>Name</Label>
+        <Input {...register("name")} />
+        {errors.name?.message && <p>{errors.root?.message}</p>}
+        <Label>Description</Label>
+        <Input {...register("description")} />
+        <Label>Budget</Label>
+        <Input type="number" {...register("budget")} />
+
+        <Label>Currency </Label>
+        <Input {...register("currency")} />
+        <Label>Status</Label>
+        <Input {...register("status")} />
+        <input type="submit" />
+      </form>
       <Button
         className="mt-4"
         size={"sm"}
